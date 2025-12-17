e12f8feaababa13850fb0ffacb4e1007d3bb3856
Adeife <adebowalejasmine437@gmail.com>
2025-12-17 20:59:19 +0100

    Added base url inside my weatherAPI.js file

--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -1,4 +1,5 @@
 const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const fetchCurrentWeather = async (city, unit = 'metric') => {
   try {
